aef297437d9d24788ae3a995910daf75dcde9e14
Kiliskiller <Kili8kr@gmx.de>
2025-07-11 23:31:35 +0200

    "first day of work on the site"

---
+++ script.js
@@ -0,0 +1,7 @@
+function showBorder(id) {
+    // document.getElementById(id).style.border = "2px";
+    document.getElementById(id).style.borderColor = "black";
+    document.getElementById(id).style.borderStyle = "solid";
+    document.getElementById(id).style.color = "orange";
+
+}
